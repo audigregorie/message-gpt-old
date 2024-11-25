07102c08120dc330a01c71a410300a7ed4e0d3c5
Audi Young <122812915+audigregorie@users.noreply.github.com>
2024-11-25 01:52:42 +0000

    rebuild

--- server/dist/config/middlewares.js
+++ server/dist/config/middlewares.js
@@ -1,10 +1,13 @@
 import cors from 'cors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import path from 'path';
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 export const setupMiddlewares = (app) => {
     app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/dist')));
         app.get('*', (req, res) => {
--- server/src/config/middlewares.ts
+++ server/src/config/middlewares.ts
@@ -2,22 +2,22 @@ import cors from 'cors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 export const setupMiddlewares = (app: any) => {
   app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
-  // const __filename = fileURLToPath(import.meta.url);
-  // const __dirname = path.dirname(__filename);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
 
-  // if (process.env.NODE_ENV === 'production') {
-  //   app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  //   app.get('*', (req: any, res: any) => {
-  //     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
-  //   });
-  // }
+    app.get('*', (req: any, res: any) => {
+      res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+    });
+  }
 
   if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
