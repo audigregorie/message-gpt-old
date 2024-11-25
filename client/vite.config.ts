aa16df38e203b22c41253fc56b1795fa2276484b
Audi Young <122812915+audigregorie@users.noreply.github.com>
2024-11-25 01:34:26 +0000

    rebuild

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/globals.scss";`
-      }
-    }
-  }
+  plugins: [react()]
 });
